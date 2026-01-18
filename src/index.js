ff45c5ae1d3387bdcd693fc5ef2e7c87a4419eb5
tepou12626 <virethy@gmail.com>
2026-01-18 00:54:41 -0500

    Updated to new Node version.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-'use strict'
-
 import * as helper from './scripts/helper.js'
 import * as preproc from './scripts/preprocess.js'
 import * as viz from './scripts/viz.js'
 import * as scales from './scripts/scales.js'
 import * as legend from './scripts/legend.js'
 import * as hover from './scripts/hover.js'
+import rankingData from './assets/data/serieA_ranking.json'
+
 
 import d3Tip from 'd3-tip'
 
@@ -16,7 +16,7 @@ import d3Tip from 'd3-tip'
  */
 
 (function (d3) {
-  
+
   let svgSize
   let graphSize
 
@@ -33,7 +33,7 @@ import d3Tip from 'd3-tip'
    *   This function handles the graph's sizing.
    */
   function setSizing(dim, selection, margin, isRadarChart) {
-    
+
     svgSize = {
       width: dim.width,
       height: dim.height
@@ -54,11 +54,14 @@ import d3Tip from 'd3-tip'
   d3.select('.chart-off-svg').call(tip)
 
   // first viz
-  d3.csv('./data_offensive.csv', d3.autoType).then(function (data) {
+  d3.csv(
+    new URL('./assets/data/data_offensive.csv', import.meta.url),
+    d3.autoType
+  ).then(function (data) {
     const marginRC1 = { top: 65, right: 35, bottom: 125, left: 35 }
 
     data = preproc.preprocessOffense(data)
-    
+
     const bounds = d3.select('#chart-off').node().getBoundingClientRect();
     const selection = d3.select(".chart-off-svg");
 
@@ -67,16 +70,16 @@ import d3Tip from 'd3-tip'
 
     var vizHeight = bounds.height > MAX_RC_HEIGHT ? MAX_RC_HEIGHT : bounds.height
     vizHeight = bounds.height < MIN_RC_HEIGHT ? MIN_RC_HEIGHT : vizHeight
-    
+
     setSizing(
-      { width: vizWidth, height: vizHeight }, 
+      { width: vizWidth, height: vizHeight },
       selection, marginRC1, true)
     buildRadarChart1()
 
     /**
      *   This function builds the graph.
      */
-    function buildRadarChart1 () {
+    function buildRadarChart1() {
       var color = d3.scaleOrdinal(['blue', 'orange'])
       color.domain(['Juventus', 'Top 7'])
       var svgElement = d3.select('.chart-off-svg')
@@ -89,14 +92,17 @@ import d3Tip from 'd3-tip'
       const bounds = d3.select('#chart-off').node().getBoundingClientRect();
       const selection = d3.select(".chart-off-svg");
       setSizing(
-        {width: Math.max(MIN_RC_WIDTH, bounds.width * 0.8), height: Math.max(MIN_RC_HEIGHT, bounds.height)}, 
+        { width: Math.max(MIN_RC_WIDTH, bounds.width * 0.8), height: Math.max(MIN_RC_HEIGHT, bounds.height) },
         selection, marginRC1, true)
       buildRadarChart1()
     })
   })
 
   // second viz
-  d3.csv('./data_defensive.csv', d3.autoType).then(function (data) {
+  d3.csv(
+    new URL('./assets/data/data_defensive.csv', import.meta.url),
+    d3.autoType
+  ).then(function (data) {
     const marginRC2 = { top: 65, right: 35, bottom: 125, left: 35 }
     data = preproc.preprocessDefense(data)
 
@@ -110,19 +116,19 @@ import d3Tip from 'd3-tip'
     vizHeight = bounds.height < MIN_RC_HEIGHT ? MIN_RC_HEIGHT : vizHeight
 
     setSizing(
-      {width: vizWidth, height: vizHeight}, 
+      { width: vizWidth, height: vizHeight },
       selection, marginRC2, true)
     buildRadarChart2()
 
     /**
      *   This function builds the graph.
      */
-    function buildRadarChart2 () {
+    function buildRadarChart2() {
       var color = d3.scaleOrdinal(['blue', 'orange'])
       color.domain(['Juventus', 'Top 7'])
       var svgElement = d3.select('.chart-def-svg')
       viz.drawDefensiveRadarChart(data, svgElement, graphSize.width, graphSize.height, marginRC2, tip)
-      legend.drawRCLegend(color, svgElement, graphSize.height, marginRC2)  
+      legend.drawRCLegend(color, svgElement, graphSize.height, marginRC2)
       viz.addButtons(svgElement, graphSize.height, marginRC2)
     }
 
@@ -131,14 +137,14 @@ import d3Tip from 'd3-tip'
       const bounds = d3.select('#chart-def').node().getBoundingClientRect();
       const selection = d3.select(".chart-def-svg");
       setSizing(
-        {width: Math.max(MIN_RC_WIDTH, bounds.width * 0.8), height: Math.max(MIN_RC_HEIGHT, bounds.height)}, 
+        { width: Math.max(MIN_RC_WIDTH, bounds.width * 0.8), height: Math.max(MIN_RC_HEIGHT, bounds.height) },
         selection, marginRC2, true)
       buildRadarChart2()
     })
   })
 
 
-  d3.json('./serieA_ranking.json', d3.autoType).then(function (data) {
+  function loadBumpChart(data) {
     const marginBC = { top: 35, right: 200, bottom: 35, left: 200 }
     const bumpRadius = 13
     const padding = 25
@@ -156,22 +162,23 @@ import d3Tip from 'd3-tip'
     const xDomain = Object.keys(data)
     const bounds = d3.select('#chart-rank').node().getBoundingClientRect();
     const selection = d3.select(".chart-rank-svg");
-    
+
     const mainG = helper.generateBCMainG(marginBC)
     const centerG = helper.appendBCCenter(mainG)
 
     helper.appendBCAxes(mainG)
-    
+
     const width = xDomain.length * 60 + marginBC.left + marginBC.right + padding * 2;
     const height = leftTeamNames.length * 60 + marginBC.top + marginBC.bottom + padding * 2;
     setSizing(
-      { width: Math.max(width, bounds.width * 0.8), 
+      {
+        width: Math.max(width, bounds.width * 0.8),
         height: Math.max(height, bounds.height)
-      }, 
+      },
       selection, marginBC, false
     );
     buildBumpChart();
-   
+
     /**
      *   This function builds the graph.
      */
@@ -200,13 +207,16 @@ import d3Tip from 'd3-tip'
       const selection = d3.select(".chart-rank-svg");
       const width = xDomain.length * 100 + marginBC.left + marginBC.right + padding * 2;
       const height = leftTeamNames.length * 60 + marginBC.top + marginBC.bottom + padding * 2;
-        setSizing(
-        { width: Math.max(width, bounds.width * 0.8), 
+      setSizing(
+        {
+          width: Math.max(width, bounds.width * 0.8),
           height: Math.max(height, bounds.height)
         }, selection, marginBC, false)
       buildBumpChart()
     })
-  })
+  }
+
+  loadBumpChart(rankingData);
 
   /*********** AFFICHAGE SCATTER PLOT***********/
   const marginSP = {
@@ -218,113 +228,122 @@ import d3Tip from 'd3-tip'
 
   const radius = 4
 
-  d3.csv('./totalSalaries2019.csv').then((data) => {
-    const g1 = helper.generateSPG1(marginSP)
+  d3.csv(new URL('./assets/data/totalSalaries2019.csv', import.meta.url))
+    .then((data) => {
+      const g1 = helper.generateSPG1(marginSP)
 
-    const bounds = d3.select('#chart-eff').node().getBoundingClientRect();
-    const selection = d3.select("#chart-eff-svg1");
-    setSizing(
-      {width: MIN_SP_WIDTH, 
-      height: MIN_SP_HEIGHT}, 
-      selection, marginSP, false
-    )
+      const bounds = d3.select('#chart-eff').node().getBoundingClientRect();
+      const selection = d3.select("#chart-eff-svg1");
+      setSizing(
+        {
+          width: MIN_SP_WIDTH,
+          height: MIN_SP_HEIGHT
+        },
+        selection, marginSP, false
+      )
 
-    helper.appendSPAxes1(g1)
-    helper.appendSPGraphLabels(g1, graphSize.width)
-    helper.placeSPTitle(g1, graphSize.width, graphSize.height)
-  
-    viz.positionSPLabels(g1, graphSize.width, graphSize.height)
+      helper.appendSPAxes1(g1)
+      helper.appendSPGraphLabels(g1, graphSize.width)
+      helper.placeSPTitle(g1, graphSize.width, graphSize.height)
 
-    viz.setSPTitleText()
-      
-    const chart1 = d3.select('#chart-eff-svg1').select('#graph-g1')
-    const axisx1 = d3.select('.x1.axis')
-    const axisy1 = d3.select('.y1.axis')
-  
-    const colorScale = scales.setSPColorScale()
-    const xScale = scales.setSPXScale(graphSize.width, data)
-    const yScale = scales.setSPYScale(graphSize.height, data)
+      viz.positionSPLabels(g1, graphSize.width, graphSize.height)
 
-    helper.drawSPXAxis(axisx1, xScale, graphSize.height)
-    helper.drawSPYAxis(axisy1, yScale, graphSize.width)
+      viz.setSPTitleText()
 
-    legend.drawSPLegend(g1, colorScale, -65, 0 )
+      const chart1 = d3.select('#chart-eff-svg1').select('#graph-g1')
+      const axisx1 = d3.select('.x1.axis')
+      const axisy1 = d3.select('.y1.axis')
 
-    buildScatterPlot(chart1, data, radius, colorScale, xScale, yScale)
-    buildDensity(chart1, data, colorScale, 'Victoire', xScale, yScale)
-    buildDensity(chart1, data, colorScale, 'Nul', xScale, yScale)
-    buildDensity(chart1, data, colorScale, 'Défaite', xScale, yScale)
-  })
+      const colorScale = scales.setSPColorScale()
+      const xScale = scales.setSPXScale(graphSize.width, data)
+      const yScale = scales.setSPYScale(graphSize.height, data)
 
-  d3.csv('./totalSalaries2020.csv').then((data) => {
-    const g2 = helper.generateSPG2(marginSP)
+      helper.drawSPXAxis(axisx1, xScale, graphSize.height)
+      helper.drawSPYAxis(axisy1, yScale, graphSize.width)
 
-    const selection = d3.select("#chart-eff-svg2");
-    setSizing(
-      {width: MIN_SP_WIDTH, 
-        height: MIN_SP_HEIGHT}, 
-      selection, marginSP, false
-    )
+      legend.drawSPLegend(g1, colorScale, -65, 0)
 
-    helper.appendSPAxes2(g2)
-    helper.appendSPGraphLabels(g2, graphSize.width)
-    helper.placeSPTitle(g2, graphSize.width, graphSize.height)
-  
-    viz.positionSPLabels(g2, graphSize.width, graphSize.height)
+      buildScatterPlot(chart1, data, radius, colorScale, xScale, yScale)
+      buildDensity(chart1, data, colorScale, 'Victoire', xScale, yScale)
+      buildDensity(chart1, data, colorScale, 'Nul', xScale, yScale)
+      buildDensity(chart1, data, colorScale, 'Défaite', xScale, yScale)
+    })
 
-    viz.setSPTitleText()
+  d3.csv(new URL('./assets/data/totalSalaries2020.csv', import.meta.url))
+    .then((data) => {
+      const g2 = helper.generateSPG2(marginSP)
 
-    const chart2 = d3.select('#chart-eff-svg2').select('#graph-g2')
-    const axisx2 = d3.select('.x2.axis')
-    const axisy2 = d3.select('.y2.axis')
+      const selection = d3.select("#chart-eff-svg2");
+      setSizing(
+        {
+          width: MIN_SP_WIDTH,
+          height: MIN_SP_HEIGHT
+        },
+        selection, marginSP, false
+      )
 
-    const colorScale = scales.setSPColorScale()
-    const xScale = scales.setSPXScale(graphSize.width, data)
-    const yScale = scales.setSPYScale(graphSize.height, data)
+      helper.appendSPAxes2(g2)
+      helper.appendSPGraphLabels(g2, graphSize.width)
+      helper.placeSPTitle(g2, graphSize.width, graphSize.height)
 
-    helper.drawSPXAxis(axisx2, xScale, graphSize.height)
-    helper.drawSPYAxis(axisy2, yScale, graphSize.width)
+      viz.positionSPLabels(g2, graphSize.width, graphSize.height)
 
-    buildScatterPlot(chart2, data, radius, colorScale, xScale, yScale)
-    buildDensity(chart2, data, colorScale, 'Victoire', xScale, yScale)
-    buildDensity(chart2, data, colorScale, 'Nul', xScale, yScale)
-    buildDensity(chart2, data, colorScale, 'Défaite', xScale, yScale)
-  })
+      viz.setSPTitleText()
 
-  d3.csv('./totalSalaries2021.csv').then((data) => {
-    const g3 = helper.generateSPG3(marginSP)
+      const chart2 = d3.select('#chart-eff-svg2').select('#graph-g2')
+      const axisx2 = d3.select('.x2.axis')
+      const axisy2 = d3.select('.y2.axis')
 
-    const selection = d3.select("#chart-eff-svg3");
-    setSizing(
-      {width: MIN_SP_WIDTH, 
-        height: MIN_SP_HEIGHT}, 
-      selection, marginSP, false
-    )
-    
-    helper.appendSPAxes3(g3)
-    helper.appendSPGraphLabels(g3, graphSize.width)
-    helper.placeSPTitle(g3, graphSize.width, graphSize.height)
+      const colorScale = scales.setSPColorScale()
+      const xScale = scales.setSPXScale(graphSize.width, data)
+      const yScale = scales.setSPYScale(graphSize.height, data)
 
-    viz.positionSPLabels(g3, graphSize.width, graphSize.height)
+      helper.drawSPXAxis(axisx2, xScale, graphSize.height)
+      helper.drawSPYAxis(axisy2, yScale, graphSize.width)
 
-    viz.setSPTitleText()
+      buildScatterPlot(chart2, data, radius, colorScale, xScale, yScale)
+      buildDensity(chart2, data, colorScale, 'Victoire', xScale, yScale)
+      buildDensity(chart2, data, colorScale, 'Nul', xScale, yScale)
+      buildDensity(chart2, data, colorScale, 'Défaite', xScale, yScale)
+    })
 
-    const chart3 = d3.select('#chart-eff-svg3').select('#graph-g3')
-    const axisx3 = d3.select('.x3.axis')
-    const axisy3 = d3.select('.y3.axis')
+  d3.csv(new URL('./assets/data/totalSalaries2021.csv', import.meta.url))
+    .then((data) => {
+      const g3 = helper.generateSPG3(marginSP)
 
-    const colorScale = scales.setSPColorScale()
-    const xScale = scales.setSPXScale(graphSize.width, data)
-    const yScale = scales.setSPYScale(graphSize.height, data)
+      const selection = d3.select("#chart-eff-svg3");
+      setSizing(
+        {
+          width: MIN_SP_WIDTH,
+          height: MIN_SP_HEIGHT
+        },
+        selection, marginSP, false
+      )
 
-    helper.drawSPXAxis(axisx3, xScale, graphSize.height)
-    helper.drawSPYAxis(axisy3, yScale, graphSize.width)
+      helper.appendSPAxes3(g3)
+      helper.appendSPGraphLabels(g3, graphSize.width)
+      helper.placeSPTitle(g3, graphSize.width, graphSize.height)
 
-    buildScatterPlot(chart3, data, radius, colorScale, xScale, yScale)
-    buildDensity(chart3, data, colorScale, 'Victoire', xScale, yScale)
-    buildDensity(chart3, data, colorScale, 'Nul', xScale, yScale)
-    buildDensity(chart3, data, colorScale, 'Défaite', xScale, yScale)
-  })
+      viz.positionSPLabels(g3, graphSize.width, graphSize.height)
+
+      viz.setSPTitleText()
+
+      const chart3 = d3.select('#chart-eff-svg3').select('#graph-g3')
+      const axisx3 = d3.select('.x3.axis')
+      const axisy3 = d3.select('.y3.axis')
+
+      const colorScale = scales.setSPColorScale()
+      const xScale = scales.setSPXScale(graphSize.width, data)
+      const yScale = scales.setSPYScale(graphSize.height, data)
+
+      helper.drawSPXAxis(axisx3, xScale, graphSize.height)
+      helper.drawSPYAxis(axisy3, yScale, graphSize.width)
+
+      buildScatterPlot(chart3, data, radius, colorScale, xScale, yScale)
+      buildDensity(chart3, data, colorScale, 'Victoire', xScale, yScale)
+      buildDensity(chart3, data, colorScale, 'Nul', xScale, yScale)
+      buildDensity(chart3, data, colorScale, 'Défaite', xScale, yScale)
+    })
 
 
   /**
@@ -339,7 +358,7 @@ import d3Tip from 'd3-tip'
    * @param {*} xScale The x scale for the graph
    * @param {*} yScale The y scale for the graph
    */
-  function buildScatterPlot (chart, data, radius, colorScale, xScale, yScale) {
+  function buildScatterPlot(chart, data, radius, colorScale, xScale, yScale) {
     viz.drawSPCircles(chart, data, radius, colorScale, xScale, yScale)
   }
 
@@ -350,7 +369,7 @@ import d3Tip from 'd3-tip'
    * @param xScaleDensity
    * @param yScaleDensity
    */
-  function buildDensity (chart, data, colorScale, result, xScaleDensity, yScaleDensity) {
+  function buildDensity(chart, data, colorScale, result, xScaleDensity, yScaleDensity) {
     viz.drawSPDensityLine(chart, data, colorScale, result, xScaleDensity, yScaleDensity)
   }
 })(d3)
